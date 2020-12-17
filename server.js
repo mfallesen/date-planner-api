537303269182a951cdc4f031325e8631cf794905
Mike Fallesen <mfallesen2010@gmail.com>
2020-12-16 17:45:09 -0800

    build basic server structure

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const express = require('express');
 const PORT = process.env.PORT || 3002;
 
 const app = express();
+
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+
+// app.use(require('./routes/api.js'));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+})
