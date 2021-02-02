c3eb7780f9cfb13fc9ad3cb22f887e4bfad99009
Mike Fallesen <mfallesen2010@gmail.com>
2021-02-01 17:22:48 -0800

    add needed express functionality

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const PORT = process.env.PORT || 3002;
 
 const app = express();
 
-// app.use(express.urlencoded({extended: true}));
-// app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
