917254e651e8c86cf2a217be487c43a1148d9cb9
Mike Fallesen <mfallesen2010@gmail.com>
2021-02-24 14:36:47 -0800

    update Cors policy

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(Cors({
-    origin: ["http://localhost:3000"]
+    origin: ["http://localhost:3000" , 'https://date-plannerly.herokuapp.com']
 }))
 app.use(require('./routes/api.js'));
 
